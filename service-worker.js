6720838731cfa865634c95ce673f90e058575a5d
excobarpg <alpertlmz001@gmail.com>
2024-10-12 15:46:54 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,43 +1,42 @@
+// Service Worker Kurulumu
 self.addEventListener('install', function(event) {
-  console.log('Service Worker: Kurulum tamamlandı.');
+  console.log('Service Worker kuruldu.');
   event.waitUntil(
-    caches.open('static-cache').then(function(cache) {
+    caches.open('ruyabet-cache').then(function(cache) {
       return cache.addAll([
-        '/offline.html', // Çevrimdışı sayfan
-        '/manifest.json', // manifest dosyan
-        '/logo.png', // logo gibi statik dosyalar
-        '/landinglogo.png'
+        '/',
+        '/index.html',
+        '/offline.html',
+        '/logo192.png',
+        '/logo512.png'
       ]);
     })
   );
 });
 
+// Service Worker Aktivasyonu
 self.addEventListener('activate', function(event) {
-  console.log('Service Worker: Aktif.');
+  console.log('Service Worker etkinleştirildi.');
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
       return Promise.all(
-        cacheNames.filter(function(cacheName) {
-          return cacheName !== 'static-cache';
-        }).map(function(cacheName) {
-          return caches.delete(cacheName);
+        cacheNames.map(function(cache) {
+          if (cache !== 'ruyabet-cache') {
+            console.log('Eski cache temizleniyor:', cache);
+            return caches.delete(cache);
+          }
         })
       );
     })
   );
 });
 
+// Fetch Event ile Cache Kullanımı
 self.addEventListener('fetch', function(event) {
   if (event.request.mode === 'navigate') {
     event.respondWith(
-      fetch(event.request).catch(() => {
+      fetch(event.request).catch(function() {
         return caches.match('offline.html');
-      })
-    );
-  } else {
-    event.respondWith(
-      caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
       })
     );
   }
