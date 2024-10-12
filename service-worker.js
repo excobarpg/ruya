2aabf832f6dd39db0e1b7675924596c39c10b4d0
excobarpg <alpertlmz001@gmail.com>
2024-10-12 15:34:19 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,15 @@
-self.addEventListener('fetch', (event) => {
+self.addEventListener('install', function(event) {
+  console.log('Service Worker: Kurulum tamamlandı.');
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('Service Worker: Aktif.');
+});
+
+self.addEventListener('fetch', function(event) {
   if (event.request.mode === 'navigate') {
-    event.respondWith(
-      (async () => {
-        return fetch('https://linkredirect-ry.com/appsite'); // Doğrudan bu linke yönlendirme yapar
-      })()
-    );
+    event.respondWith(fetch(event.request).catch(() => {
+      return caches.match('offline.html');
+    }));
   }
 });
