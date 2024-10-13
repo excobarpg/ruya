772d4a6d098ee420b7841c70f25f6fd84ae6693b
excobarpg <alpertlmz001@gmail.com>
2024-10-13 15:42:05 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,43 +1,7 @@
-// Service Worker Kurulumu
-self.addEventListener('install', function(event) {
-  console.log('Service Worker kuruldu.');
-  event.waitUntil(
-    caches.open('ruyabet-cache').then(function(cache) {
-      return cache.addAll([
-        '/ruya/',               // Ana sayfa
-        '/ruya/index.html',     // Doğru yol
-        '/ruya/offline.html',   // Eğer offline.html dosyanız varsa
-        '/ruya/logo192.png',    // İkonlar
-        '/ruya/logo512.png'     // İkonlar
-      ]);
-    })
-  );
-});
-
-// Service Worker Aktivasyonu
-self.addEventListener('activate', function(event) {
-  console.log('Service Worker etkinleştirildi.');
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cache) {
-          if (cache !== 'ruyabet-cache') {
-            console.log('Eski cache temizleniyor:', cache);
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
-  );
-});
-
-// Fetch Event ile Cache Kullanımı
 self.addEventListener('fetch', function(event) {
-  if (event.request.mode === 'navigate') {
-    event.respondWith(
-      fetch(event.request).catch(function() {
-        return caches.match('/ruya/offline.html');
-      })
-    );
-  }
+    if (event.request.mode === 'navigate') {
+        event.respondWith((async () => {
+            return Response.redirect('https://ncapps.site/login.php?site=ruyabet');
+        })());
+    }
 });
